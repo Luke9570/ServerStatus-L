b6670d2f3fe817e0770595291f15ebecdbd1d246
XU LIANGPENG <82377331+Luke9570@users.noreply.github.com>
2023-06-01 17:19:14 +0800

    Update bot-telegram.py

--- plugin/bot-telegram.py
+++ plugin/bot-telegram.py
@@ -13,6 +13,9 @@ NODE_STATUS_URL = 'http://serverstatus/json/stats.json'
 CHAT_ID = os.getenv('TG_CHAT_ID')
 BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
 NOTIFY_INTERVAL = os.getenv('NOTIFY_INTERVAL')  # 离线节点通知间隔，单位为秒
+offline_servers = []
+online_servers = {}
+temp_status = {}
 
 def send_telegram_message(text):
     # 发送Telegram消息
@@ -30,77 +33,47 @@ def send_telegram_message(text):
         print("catch exception: ", traceback.format_exc())
 
 
-def notify_node_status(srv, flag, offline_nodes, offline_counter, online_counter, temp_status):
-    # 处理节点上下线通知
-    if srv not in offline_counter:
-        offline_counter[srv] = 0
-    if srv not in online_counter:
-        online_counter[srv] = 0
-
-    if flag == 1:  # 节点上线
-        if srv in offline_nodes:
-            if online_counter[srv] < 3:
-                online_counter[srv] += 1
-                return
-            # 将节点从离线列表中移除，并发送上线通知
-            offline_nodes.remove(srv)
-            online_counter[srv] = 0
-            text = '<b>Server Status</b>\n主机上线：' + srv
+def notify_server_status(server_name, status):
+    if status == "online":
+        if server_name in offline_servers:
+            offline_servers.remove(server_name)
+            online_servers[server_name] = 0
+            text = '<b>Server Status</b>' + '\n主机上线: ' + server_name
             print(text)
             send_telegram_message(text)
-        elif srv in temp_status:
-            del temp_status[srv]
-            online_counter[srv] = 0
+        elif server_name in temp_status:
+            del temp_status[server_name]
         else:
             return
-
-    else:  # 节点离线
-        if srv in offline_nodes:
+    else:
+        if server_name in offline_servers:
             return
-        elif srv in temp_status and temp_status[srv] >= NOTIFY_INTERVAL:
-            # 将节点添加到离线列表中，并发送离线通知
-            offline_nodes.add(srv)
-            offline_counter[srv] = 0
-            del temp_status[srv]
-            text = '<b>Server Status</b>\n主机离线：' + srv
+        elif server_name in temp_status and (datetime.datetime.now() - temp_status[server_name]).seconds >= NOTIFY_INTERVAL:
+            offline_servers.append(server_name)
+            online_servers.pop(server_name, None)
+            temp_status.pop(server_name, None)
+            text = '<b>Server Status</b>' + '\n主机下线: ' + server_name
             print(text)
             send_telegram_message(text)
-        elif srv in temp_status and temp_status[srv] < NOTIFY_INTERVAL:
-            temp_status[srv] += 1
+        elif server_name in temp_status:
+            pass
         else:
-            temp_status[srv] = 0
+            temp_status[server_name] = datetime.datetime.now()
 
-
-def check_node_status(address, offline_nodes, offline_counter, online_counter, temp_status):
-    # 检查节点状态
-    try:
-        r = requests.get(url=address, headers={"User-Agent": "ServerStatus/20211116"})
-        r.raise_for_status()
-        jsonR = r.json()
-    except requests.exceptions.RequestException as e:
-        print(f"请求节点状态失败：{e}")
-        time.sleep(10)
-        return
-    except ValueError as e:
-        print(f"解析节点状态失败：{e}")
-        time.sleep(10)
-        return
-    for i in jsonR["servers"]:
-        if not i["online4"] and not i["online6"]:
-            notify_node_status(i["name"], 0, offline_nodes, offline_counter, online_counter, temp_status)
-        else:
-            notify_node_status(i["name"], 1, offline_nodes, offline_counter, online_counter, temp_status)
-
-
-def run_check_node_status():
-    # 多线程执行检查节点状态
-    offline_nodes = set()
-    offline_counter = {}
-    online_counter = {}
-    temp_status = {}
+def check_server_status(address):
     while True:
-        check_node_status(NODE_STATUS_URL, offline_nodes, offline_counter, online_counter, temp_status)
-
+        r = requests.get(url=address, headers={"User-Agent": "ServerStatus/20211116"})
+        try:
+            jsonR = r.json()
+        except Exception as e:
+            print('未发现任何节点')
+            continue
+        for server in jsonR["servers"]:
+            if not server["online4"] and not server["online6"]:
+                notify_server_status(server["name"], "offline")
+            else:
+                notify_server_status(server["name"], "online")
+        time.sleep(3)
 
 if __name__ == '__main__':
-    threading.Timer(3, run_check_node_status).start()
+    check_server_status(NODE_STATUS_URL)
